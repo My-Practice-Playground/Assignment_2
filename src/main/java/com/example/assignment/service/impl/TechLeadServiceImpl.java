94080df01005a2de98dde7fb5ffa985edd09a7d5
Shan <perera.alc2000@gmail.com>
2023-09-17 01:22:09 +0530

    tl service impl  added

--- src/main/java/com/example/assignment/service/impl/TechLeadServiceImpl.java
+++ src/main/java/com/example/assignment/service/impl/TechLeadServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.assignment.service.impl;
 
+import com.example.assignment.dto.TechLeadDto;
 import com.example.assignment.service.TechLeadService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,4 +8,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class TechLeadServiceImpl implements TechLeadService {
+    @Override
+    public String save(TechLeadDto dto) {
+        return null;
+    }
+
+    @Override
+    public void update(TechLeadDto dto) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public TechLeadDto view(String id) {
+        return null;
+    }
 }
