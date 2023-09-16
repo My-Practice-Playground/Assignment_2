03531af94f72a6523661e167beee06389c4c9ca8
Shan <perera.alc2000@gmail.com>
2023-09-17 01:20:27 +0530

    custom documentation added

--- src/main/java/com/example/assignment/dto/ProjectDto.java
+++ src/main/java/com/example/assignment/dto/ProjectDto.java
@@ -1,5 +1,7 @@
 package com.example.assignment.dto;
 
-
+/**
+ * DTO for {@link com.example.assignment.entity.custom.Project}
+ */
 public record ProjectDto(String id, String description, Double price, TechLeadDto techlead) {
 }
--- src/main/java/com/example/assignment/dto/TechLeadDto.java
+++ src/main/java/com/example/assignment/dto/TechLeadDto.java
@@ -2,5 +2,8 @@ package com.example.assignment.dto;
 
 import java.util.List;
 
+/**
+ * DTO for {@link com.example.assignment.entity.custom.TechLead}
+ */
 public record TechLeadDto(String id, String name, Double salary, List<ProjectDto> projects) {
 }
