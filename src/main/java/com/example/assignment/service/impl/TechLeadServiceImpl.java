5062fcc6812799c71de8ec82f9cc9ddac21f99ca
Shan <perera.alc2000@gmail.com>
2023-09-17 01:21:56 +0530

    tl service impl  added

---
+++ src/main/java/com/example/assignment/service/impl/TechLeadServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.assignment.service.impl;
+
+import com.example.assignment.service.TechLeadService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class TechLeadServiceImpl implements TechLeadService {
+}
