6e2a832ce19c25df9858f72db06bba7d3a77c7fb
Shan <perera.alc2000@gmail.com>
2023-09-19 14:03:58 +0530

    custom exceptions added in service layer

--- src/main/java/com/example/assignment/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/example/assignment/service/impl/ProjectServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.assignment.dto.TechLeadDto;
 import com.example.assignment.entity.custom.Project;
 import com.example.assignment.repo.ProjectRepository;
 import com.example.assignment.service.ProjectService;
+import com.example.assignment.service.util.ProjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,33 +21,31 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public String save(ProjectDto dto) {
-        System.out.print("service impl: " + dto);
         Project entity = new Project();
         entity.setId(dto.id());
         entity.setDescription(dto.description());
         entity.setPrice(dto.price());
-
         projectRepository.save(entity);
         return "done";
     }
 
     @Override
-    public void update(ProjectDto dto) {
-        if (!projectRepository.existsById(dto.id())) throw new RuntimeException("Project not found !");
+    public void update(ProjectDto dto) throws ProjectNotFoundException{
+        if (!projectRepository.existsById(dto.id())) throw new ProjectNotFoundException();
         projectRepository.updateDescriptionAndPriceById(dto.description(), dto.price(), dto.id());
     }
 
     @Override
-    public void delete(String id) {
-        if (!projectRepository.existsById(id)) throw new RuntimeException("Project not found !");
+    public void delete(String id) throws ProjectNotFoundException{
+        if (!projectRepository.existsById(id)) throw new ProjectNotFoundException();
         Project project = new Project();
         project.setId(id);
         projectRepository.delete(project);
     }
 
     @Override
-    public ProjectDto view(String id) {
-        if (!projectRepository.existsById(id)) throw new RuntimeException("Project not found !");
+    public ProjectDto view(String id) throws ProjectNotFoundException{
+        if (!projectRepository.existsById(id)) throw new ProjectNotFoundException();
         Project project = projectRepository.getProjectById(id);
         return new ProjectDto(project.getId(), project.getDescription(), project.getPrice(), getTechlead(project));
     }
---
+++ src/main/java/com/example/assignment/service/util/ProjectNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.assignment.service.util;
+
+public class ProjectNotFoundException extends RuntimeException{
+}
